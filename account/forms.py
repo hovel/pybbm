2860f16ec0956a1fb46f2a5b052e4ce98fe5c798
Grigoriy Petukhov <lizendir@gmail.com>
2008-09-19 23:01:26 +0700

    fix newforms issue

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,7 @@
 
 import re
 
-from django import newforms as forms
+from django import forms
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth import authenticate, login
