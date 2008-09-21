b4fcaacec6cb2ff498fa5475d63f6648a803adbb
Grigoriy Petukhov <lizendir@gmail.com>
2008-09-22 00:39:21 +0700

    add search via google

--- settings.py
+++ settings.py
@@ -90,6 +90,14 @@ INSTALLED_APPS = (
 
 FORCE_SCRIPT_NAME = ''
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
+
 # django-account settings
 ACCOUNT_ACTIVATION = False
 LOGIN_REDIRECT_URL = '/'
