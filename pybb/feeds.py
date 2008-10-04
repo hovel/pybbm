bd24ee22476ba6de2dba2996d14753071d6e7e45
Grigoriy Petukhov <lizendir@gmail.com>
2008-10-04 20:02:01 +0700

    tuned posts feed

--- pybb/feeds.py
+++ pybb/feeds.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.feeds import Feed
 from django.utils.feedgenerator import Atom1Feed
+from django.core.urlresolvers import reverse
 
 from pybb.models import Post
 
@@ -7,9 +8,13 @@ class LastPosts(Feed):
     feed_type = Atom1Feed
     title = 'Latest posts on forum'
     description = 'Latest posts on forum'
-    link = 'adsf'
+    def link(self):
+        return reverse('pybb.views.index')
     title_template = 'pybb/feeds/posts_title.html'
     description_template = 'pybb/feeds/posts_description.html'
 
     def items(self):
         return Post.objects.order_by('-created')[:15]
+
+    def item_guid(self, obj):
+        return str(obj.id)
