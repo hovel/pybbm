daae913feae7ac3812b89a5e303c418eb8a9e694
Grigoriy Petukhov <lizendir@gmail.com>
2009-01-20 19:15:41 +0600

    set empty default db settings in settings.py; add debug_toolbar to ignore list

--- settings.py
+++ settings.py
@@ -14,9 +14,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'pybb'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'web'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'web-**'         # Not used with sqlite3.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
