b02fbad6ab9b58636049fa3d001ac435f445e48f
nikolai.sevostjanov@gmail.com <nikolai.sevostjanov@gmail.com>
2009-01-27 10:24:02 +0600

    improve admin interface

--- pybb/admin.py
+++ pybb/admin.py
@@ -1,23 +1,92 @@
 # -*- coding: utf-8
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from pybb.models import Category, Forum, Topic, Post, Profile, Read
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'position', 'forum_count']
+    list_per_page = 20
+    ordering = ['position']
+    search_fields = ['name']
 
 class ForumAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'position', 'topic_count']
-
+    list_per_page = 20
+    ordering = ['-category']
+    search_fields = ['name', 'category__name']
+    fieldsets = (
+        (None, {
+                'fields': ('category', 'name', 'updated')
+                }
+         ),
+        (_('Additional options'), {
+                'classes': ('collapse',),
+                'fields': ('position', 'description', 'post_count', 'moderators')
+                }
+            ),
+        )
+    
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name', 'forum', 'created', 'head', 'post_count']
+    list_per_page = 20
+    ordering = ['-created']
+    date_hierarchy = 'created'
     search_fields = ['name']
+    fieldsets = (
+        (None, {
+                'fields': ('forum', 'name', 'user', ('created', 'updated'))
+                }
+         ),
+        (_('Additional options'), {
+                'classes': ('collapse',),
+                'fields': (('views', 'post_count'), ('sticky', 'closed'), 'subscribers')
+                }
+         ),
+        )
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['topic', 'user', 'created', 'updated', 'summary']
+    list_per_page = 20
+    ordering = ['-created']
+    date_hierarchy = 'created'
     search_fields = ['body']
+    fieldsets = (
+        (None, {
+                'fields': ('topic', 'user', 'markup')
+                }
+         ),
+        (_('Additional options'), {
+                'classes': ('collapse',),
+                'fields' : (('created', 'updated'), 'user_ip')
+                }
+         ),
+        (_('Message'), {
+                'fields': ('body', 'body_html', 'body_text')
+                }
+         ),
+        )
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'time_zone', 'location', 'language']
+    list_per_page = 20
+    ordering = ['-user']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+    fieldsets = (
+        (None, {
+                'fields': ('user', 'time_zone', 'markup', 'location', 'language')
+                }
+         ),
+        (_('IM'), {
+                'classes': ('collapse',),
+                'fields' : ('jabber', 'icq', 'msn', 'aim', 'yahoo')
+                }
+         ),
+        (_('Additional options'), {
+                'classes': ('collapse',),
+                'fields' : ('site', 'avatar', 'signature', 'show_signatures')
+                }
+         ),
+        )
 
 class ReadAdmin(admin.ModelAdmin):
     list_display = ['user', 'topic', 'time']
