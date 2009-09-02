ca870a426b903b61744c416637ae962fc760f3a4
denger <denger@denger.pp.ru>
2009-09-02 15:41:18 +0700

    ajax delete post, fix js and css MEDIA_URL address

---
+++ pybb/static/pybb/js/pybbjs.js
@@ -0,0 +1,12 @@
+function pybb_delete_post(url, post_id, confirm_text){
+    conf = confirm(confirm_text);
+    if (!conf) return false;
+    obj =  {url: url,
+            type: 'POST',
+            dataType: 'text',
+            success: function (data, textStatus){
+                    $("#"+post_id).slideUp();
+                }
+            }
+    $.ajax(obj);
+}
