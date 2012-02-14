4f477bf1e9e77634345763b790858a702c662723
Nathaniel Tucker <me@ntucker.me>
2012-02-13 16:15:21 -0800

    Support django-notification

--- pybb/subscription.py
+++ pybb/subscription.py
@@ -5,34 +5,49 @@ from django.utils import translation
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 from django import forms
+from django.conf import settings
+
+if "notification" in settings.INSTALLED_APPS:
+    from notification import models as notification
+else:
+    notification = None
+
 
 email_validator = forms.EmailField()
 
 def notify_topic_subscribers(post):
     topic = post.topic
     if post != topic.head:
-        for user in topic.subscribers.all():
-            if user != post.user:
-                try:
-                    email_validator.clean(user.email)
-                except:
-                    #invalid email
-                    continue
-                old_lang = translation.get_language()
-                lang = user.get_profile().language or dict(settings.LANGUAGES)[settings.LANGUAGE_CODE.split('-')[0]]
-                translation.activate(lang)
-                delete_url = reverse('pybb:delete_subscription', args=[post.topic.id])
-                current_site = Site.objects.get_current()
-                subject = render_to_string('pybb/mail_templates/subscription_email_subject.html',
-                                           { 'site': current_site,
-                                             'post': post
-                                           })
-                # Email subject *must not* contain newlines
-                subject = ''.join(subject.splitlines())
-                message = render_to_string('pybb/mail_templates/subscription_email_body.html',
-                                           { 'site': current_site,
-                                             'post': post,
-                                             'delete_url': delete_url,
-                                             })
-                send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email], fail_silently=True)
-                translation.activate(old_lang)
+        delete_url = reverse('pybb:delete_subscription', args=[post.topic.id])
+        current_site = Site.objects.get_current()
+        if notification:
+            notification.send(topic.subscribers.exclude(pk=post.user.pk),
+                              "forum_subscription_reply",
+                              {'site': current_site,
+                               'post': post,
+                               'delete_url': delete_url,
+                               })
+        else:
+            for user in topic.subscribers.all():
+                if user != post.user:
+                    try:
+                        email_validator.clean(user.email)
+                    except:
+                        #invalid email
+                        continue
+                    old_lang = translation.get_language()
+                    lang = user.get_profile().language or dict(settings.LANGUAGES)[settings.LANGUAGE_CODE.split('-')[0]]
+                    translation.activate(lang)
+                    subject = render_to_string('pybb/mail_templates/subscription_email_subject.html',
+                                               { 'site': current_site,
+                                                 'post': post
+                                               })
+                    # Email subject *must not* contain newlines
+                    subject = ''.join(subject.splitlines())
+                    message = render_to_string('pybb/mail_templates/subscription_email_body.html',
+                                               { 'site': current_site,
+                                                 'post': post,
+                                                 'delete_url': delete_url,
+                                                 })
+                    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email], fail_silently=True)
+                    translation.activate(old_lang)
