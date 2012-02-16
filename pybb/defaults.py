2b9d31c4f6b11e68fc82a46e632e398263539cae
Pavel Zhukov <gelios@gmail.com>
2012-02-16 23:48:13 +0400

    Fix #5

--- pybb/defaults.py
+++ pybb/defaults.py
@@ -18,7 +18,8 @@ PYBB_ATTACHMENT_SIZE_LIMIT = getattr(settings, 'PYBB_ATTACHMENT_SIZE_LIMIT', 102
 PYBB_ATTACHMENT_ENABLE = getattr(settings, 'PYBB_ATTACHMENT_ENABLE', False)
 PYBB_ATTACHMENT_UPLOAD_TO = getattr(settings, 'PYBB_ATTACHMENT_UPLOAD_TO', join('pybb_upload', 'attachments'))
 
-PYBB_DEFAULT_AVATAR_URL = getattr(settings, 'STATIC_URL', '') + 'pybb/img/default_avatar.jpg'
+PYBB_DEFAULT_AVATAR_URL = getattr(settings,'PYBB_DEFAULT_AVATAR_URL',
+    getattr(settings, 'STATIC_URL', '') + 'pybb/img/default_avatar.jpg')
 
 PYBB_DEFAULT_TITLE = getattr(settings, 'PYBB_DEFAULT_TITLE', 'PYBB Powered Forum')
 
