e3f1286133d96807b4d959ab69b566dc38ebe809
Filip Wasilewski <en@ig.ma>
2012-06-30 17:27:01 +0200

    Replaced settings-based button styling with template includes.
    The PYBB_BUTTONS setting is removed and overridable `pybb/_button_*.html`
    templates for `save`, `new topic` and `submit` buttons are provided in case
    css styling methods are not enough.
    This is a backwards-incompatible change.

--- pybb/context_processors.py
+++ pybb/context_processors.py
@@ -9,7 +9,6 @@ def processor(request):
     context = {}
     for i in (
         'PYBB_TEMPLATE',
-        'PYBB_BUTTONS',
         'PYBB_DEFAULT_AVATAR_URL',
         'PYBB_MARKUP',
         'PYBB_DEFAULT_TITLE',
