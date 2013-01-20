0c680de79e538dd778d60831c4c46d4f69a82593
Sergey Fursov <GeyseR85@gmail.com>
2013-01-20 05:22:32 +0400

    fix when poll answers is empty, render non_form_errors

--- pybb/forms.py
+++ pybb/forms.py
@@ -43,7 +43,7 @@ class PollAnswerForm(forms.ModelForm):
 class BasePollAnswerFormset(BaseInlineFormSet):
     def clean(self):
         if any(self.errors):
-            return
+            raise forms.ValidationError(self.errors)
         forms_cnt = len(self.initial_forms) + len([form for form in self.extra_forms if form.has_changed()]) -\
                     len(self.deleted_forms)
         if forms_cnt > defaults.PYBB_POLL_MAX_ANSWERS:
