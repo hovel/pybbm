cf4accf7b9b3a91068d521e2141f5da97f1583b6
Sergey Fursov <GeyseR85@gmail.com>
2013-03-31 20:41:48 +0400

    removed registration dependency from test project

--- test/test_project/test_urls.py
+++ test/test_project/test_urls.py
@@ -12,7 +12,6 @@ urlpatterns = patterns('',
 
     # TODO: we have to break such dependencies in application
     url(r"^accounts/password/$", 'django.contrib.auth.views.password_change', name="auth_password_change"),
-    url(r"^accounts/signup/$", 'django.contrib.auth.views.login', name="registration_register", kwargs={'template_name': 'base.html'}),
     url(r"^accounts/login/$", 'django.contrib.auth.views.login', name="auth_login", kwargs={'template_name': 'base.html'}),
 
     url(r'^', include('pybb.urls', namespace='pybb')),
