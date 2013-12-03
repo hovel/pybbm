1585d51a86ddfb8a1a4cfc84a4c6fa502e2926f2
Sergey Fursov <GeyseR85@gmail.com>
2013-12-04 00:29:30 +0400

    open files in tests as bytes

--- pybb/tests.py
+++ pybb/tests.py
@@ -1132,7 +1132,7 @@ class AttachmentTest(TestCase, SharedTestModule):
         defaults.PYBB_ATTACHMENT_ENABLE = True
         self.ORIG_PYBB_PREMODERATION = defaults.PYBB_PREMODERATION
         defaults.PYBB_PREMODERATION = False
-        self.file = open(os.path.join(os.path.dirname(__file__), 'static', 'pybb', 'img', 'attachment.png'))
+        self.file_name = os.path.join(os.path.dirname(__file__), 'static', 'pybb', 'img', 'attachment.png')
         self.create_user()
         self.create_initial()
 
@@ -1140,10 +1140,11 @@ class AttachmentTest(TestCase, SharedTestModule):
         add_post_url = reverse('pybb:add_post', kwargs={'topic_id': self.topic.id})
         self.login_client()
         response = self.client.get(add_post_url)
-        values = self.get_form_values(response)
-        values['body'] = 'test attachment'
-        values['attachments-0-file'] = self.file
-        response = self.client.post(add_post_url, values, follow=True)
+        with open(self.file_name, 'rb') as fp:
+            values = self.get_form_values(response)
+            values['body'] = 'test attachment'
+            values['attachments-0-file'] = fp
+            response = self.client.post(add_post_url, values, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Post.objects.filter(body='test attachment').exists())
 
@@ -1151,13 +1152,14 @@ class AttachmentTest(TestCase, SharedTestModule):
         add_post_url = reverse('pybb:add_post', kwargs={'topic_id': self.topic.id})
         self.login_client()
         response = self.client.get(add_post_url)
-        values = self.get_form_values(response)
-        values['body'] = 'test attachment'
-        values['attachments-0-file'] = self.file
-        del values['attachments-INITIAL_FORMS']
-        del values['attachments-TOTAL_FORMS']
-        with self.assertRaises(ValidationError):
-            self.client.post(add_post_url, values, follow=True)
+        with open(self.file_name, 'rb') as fp:
+            values = self.get_form_values(response)
+            values['body'] = 'test attachment'
+            values['attachments-0-file'] = fp
+            del values['attachments-INITIAL_FORMS']
+            del values['attachments-TOTAL_FORMS']
+            with self.assertRaises(ValidationError):
+                self.client.post(add_post_url, values, follow=True)
 
     def tearDown(self):
         defaults.PYBB_ATTACHMENT_ENABLE = self.PYBB_ATTACHMENT_ENABLE
