2de6d8a2f503dbaae8faa056000b10b14818208a
James Keys <skolsuper@gmail.com>
2015-05-01 11:36:06 +0700

    Expose PybbProfile base class in models submodule and make both profile classes available in their old location

--- pybb/models/__init__.py
+++ pybb/models/__init__.py
@@ -3,7 +3,7 @@ from .category import Category
 from .forum import Forum
 from .poll_answer import PollAnswer, PollAnswerUser
 from .post import Post
-from .profile import Profile
+from .profile import PybbProfile, Profile
 from .read_trackers import ForumReadTracker, TopicReadTracker
 from .renderable import RenderableItem
 from .topic import Topic
