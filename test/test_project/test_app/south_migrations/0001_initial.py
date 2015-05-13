41d54196ef822ed9392a7181dae3f143e38bac93
Sergey Fursov <geyser85@gmail.com>
2015-05-13 14:56:03 +0300

    removed explicit unicode str prefix form test_app south migration

--- test/test_project/test_app/south_migrations/0001_initial.py
+++ test/test_project/test_app/south_migrations/0001_initial.py
@@ -14,8 +14,8 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         if AUTH_USER == 'test_app.CustomUser':
             # Adding model 'CustomUser'
-            db.create_table(u'test_app_customuser', (
-                (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            db.create_table('test_app_customuser', (
+                ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
                 ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
                 ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
                 ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
@@ -25,29 +25,29 @@ class Migration(SchemaMigration):
                 ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
                 ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ))
-            db.send_create_signal(u'test_app', ['CustomUser'])
+            db.send_create_signal('test_app', ['CustomUser'])
 
             # Adding M2M table for field groups on 'CustomUser'
-            m2m_table_name = db.shorten_name(u'test_app_customuser_groups')
+            m2m_table_name = db.shorten_name('test_app_customuser_groups')
             db.create_table(m2m_table_name, (
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-                ('customuser', models.ForeignKey(orm[u'test_app.customuser'], null=False)),
-                ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+                ('customuser', models.ForeignKey(orm['test_app.customuser'], null=False)),
+                ('group', models.ForeignKey(orm['auth.group'], null=False))
             ))
             db.create_unique(m2m_table_name, ['customuser_id', 'group_id'])
 
             # Adding M2M table for field user_permissions on 'CustomUser'
-            m2m_table_name = db.shorten_name(u'test_app_customuser_user_permissions')
+            m2m_table_name = db.shorten_name('test_app_customuser_user_permissions')
             db.create_table(m2m_table_name, (
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-                ('customuser', models.ForeignKey(orm[u'test_app.customuser'], null=False)),
-                ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+                ('customuser', models.ForeignKey(orm['test_app.customuser'], null=False)),
+                ('permission', models.ForeignKey(orm['auth.permission'], null=False))
             ))
             db.create_unique(m2m_table_name, ['customuser_id', 'permission_id'])
 
         # Adding model 'CustomProfile'
-        db.create_table(u'test_app_customprofile', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('test_app_customprofile', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('signature', self.gf('django.db.models.fields.TextField')(max_length=1024, blank=True)),
             ('signature_html', self.gf('django.db.models.fields.TextField')(max_length=1054, blank=True)),
             ('time_zone', self.gf('django.db.models.fields.FloatField')(default=3.0)),
@@ -56,76 +56,76 @@ class Migration(SchemaMigration):
             ('post_count', self.gf('django.db.models.fields.IntegerField')(default=0, blank=True)),
             ('avatar', self.gf(get_image_field_full_name())(max_length=100, null=True, blank=True)),
             ('autosubscribe', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name=u'pybb_customprofile', unique=True, to=orm[AUTH_USER])),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='pybb_customprofile', unique=True, to=orm[AUTH_USER])),
         ))
-        db.send_create_signal(u'test_app', ['CustomProfile'])
+        db.send_create_signal('test_app', ['CustomProfile'])
 
 
     def backwards(self, orm):
         if AUTH_USER == 'test_app.CustomUser':
             # Deleting model 'CustomUser'
-            db.delete_table(u'test_app_customuser')
+            db.delete_table('test_app_customuser')
 
             # Removing M2M table for field groups on 'CustomUser'
-            db.delete_table(db.shorten_name(u'test_app_customuser_groups'))
+            db.delete_table(db.shorten_name('test_app_customuser_groups'))
 
             # Removing M2M table for field user_permissions on 'CustomUser'
-            db.delete_table(db.shorten_name(u'test_app_customuser_user_permissions'))
+            db.delete_table(db.shorten_name('test_app_customuser_user_permissions'))
 
         # Deleting model 'CustomProfile'
-        db.delete_table(u'test_app_customprofile')
+        db.delete_table('test_app_customprofile')
 
 
     models = {
-        u'auth.group': {
+        'auth.group': {
             'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        u'contenttypes.contenttype': {
+        'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        u'test_app.customprofile': {
+        'test_app.customprofile': {
             'Meta': {'object_name': 'CustomProfile'},
             'autosubscribe': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'avatar': (get_image_field_full_name(), [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'language': ('django.db.models.fields.CharField', [], {'default': "'en-us'", 'max_length': '10', 'blank': 'True'}),
             'post_count': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
             'show_signatures': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'signature': ('django.db.models.fields.TextField', [], {'max_length': '1024', 'blank': 'True'}),
             'signature_html': ('django.db.models.fields.TextField', [], {'max_length': '1054', 'blank': 'True'}),
             'time_zone': ('django.db.models.fields.FloatField', [], {'default': '3.0'}),
-            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "u'pybb_customprofile'", 'unique': 'True', 'to': u"orm['%s']" % AUTH_USER})
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'pybb_customprofile'", 'unique': 'True', 'to': "orm['%s']" % AUTH_USER})
         },
 
     }
 
     if AUTH_USER == 'test_app.CustomUser':
-        models[u'test_app.customuser'] = {
+        models['test_app.customuser'] = {
             'Meta': {'object_name': 'CustomUser'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': "orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': "orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
         }
     else:
