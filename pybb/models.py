3982c0b878cce6d8087aaf0e3eff54bd16e2af27
Dmitriy Sintsov <questpc256@gmail.com>
2016-09-05 12:23:42 +0300

    Removed null=True from pybb.models.Forum.moderators arguments which caused 'null has no effect on ManyToManyField' error.

--- pybb/models.py
+++ pybb/models.py
@@ -67,7 +67,7 @@ class Forum(models.Model):
     name = models.CharField(_('Name'), max_length=80)
     position = models.IntegerField(_('Position'), blank=True, default=0)
     description = models.TextField(_('Description'), blank=True)
-    moderators = models.ManyToManyField(get_user_model_path(), blank=True, null=True, verbose_name=_('Moderators'))
+    moderators = models.ManyToManyField(get_user_model_path(), blank=True, verbose_name=_('Moderators'))
     updated = models.DateTimeField(_('Updated'), blank=True, null=True)
     post_count = models.IntegerField(_('Post count'), blank=True, default=0)
     topic_count = models.IntegerField(_('Topic count'), blank=True, default=0)
