0cad204f4a0ff817b14b7075d6f95611359393fd
DylannCordel <d.cordel@webu.coop>
2017-01-26 18:49:56 +0100

    allow pybb_topic_poll_not_voted to be used with AAnonymousUser

--- pybb/templatetags/pybb_tags.py
+++ pybb/templatetags/pybb_tags.py
@@ -200,6 +200,8 @@ def pybb_topic_inline_pagination(topic):
 
 @register.filter
 def pybb_topic_poll_not_voted(topic, user):
+    if user.is_anonymous():
+        return True
     return not PollAnswerUser.objects.filter(poll_answer__topic=topic, user=user).exists()
 
 
