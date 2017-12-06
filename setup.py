82b630e8cd5eae34706d80bd6ad0b1769c213f3f
lampslave <lampslave@gmail.com>
2017-12-06 06:49:46 +0300

    release 0.19.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybbm',
-    version='0.18.4',
+    version='0.19.0',
     description='PyBB Modified. Django forum application',
     long_description=open('README.rst').read(),
     author='Pavel Zhukov',
