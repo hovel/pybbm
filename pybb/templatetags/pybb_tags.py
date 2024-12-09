63897e525bddc3e535cb3c7ca469bc5aeac95aa0
Hajra Shoaib <devv.hajrashoaib@gmail.com>
2024-12-09 20:51:28 +0500

    Replaced deprecated translation methods

--- pybb/templatetags/pybb_tags.py
+++ pybb/templatetags/pybb_tags.py
@@ -10,7 +10,7 @@ from django.core.cache import cache
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_text
 from django.utils.html import escape
-from django.utils.translation import ugettext as _, ungettext
+from django.utils.translation import gettext as _, ungettext
 from django.utils import dateformat
 from django.utils.timezone import timedelta
 from django.utils.timezone import now as tznow
